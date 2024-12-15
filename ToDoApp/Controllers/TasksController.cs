f8b8cfbdbf766808306ddbf403776e3e0cd23218
Kirill <ishardcoded@gmail.com>
2024-12-15 12:37:55 +0300

    Написали CRUD операции.

--- ToDoApp/Controllers/TasksController.cs
+++ ToDoApp/Controllers/TasksController.cs
@@ -16,9 +16,49 @@ namespace ToDoApp.Controllers
         }
 
         [HttpGet]
-        public Task[] GetTasks()
+        public Task[] Get()
         {
             return dbContext.Tasks.ToArray();
-        } 
+        }
+
+        // /tasks/id
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var task = dbContext.Tasks.SingleOrDefault(x=>x.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public void Add([FromBody] Task task)
+        {
+            dbContext.Tasks.Add(task);
+            dbContext.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var task = dbContext.Tasks.SingleOrDefault(x => x.Id == id);
+            if (task != null)
+            {
+                dbContext.Tasks.Remove(task);
+                dbContext.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public void Put(Task task)
+        {
+            dbContext.Tasks.Update(task);
+            dbContext.SaveChanges();
+        }
     }
 }
