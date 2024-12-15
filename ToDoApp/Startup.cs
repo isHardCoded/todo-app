e471e82e43ba8b43a1a5151ffd88b3f7c52ed3fb
Kirill <ishardcoded@gmail.com>
2024-12-15 09:38:39 +0300

    Добавили useRouting()

--- ToDoApp/Startup.cs
+++ ToDoApp/Startup.cs
@@ -10,10 +10,11 @@
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
         {
             
+            app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", () => "Hello World!");
-                endpoints.MapControllers();
+                // endpoints.MapControllers();
             });
         }
     }
